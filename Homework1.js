ff4aea626af9ee5ef542adee4ecc43c68090dc2e
vozzhaevamv <mashazhukova2311@gmail.com>
2024-11-06 20:22:46 +1000

    Homework1.js

--- Homework1.js
+++ Homework1.js
@@ -7,13 +7,13 @@ const n4 = 12;
 const n5 = 15;
 const n6 = 18;
 
-const mainDet = n1 * n5 - n2 * n4; //Основной определитель
+const D = n1 * n5 - n2 * n4; //Основной определитель
 
-const xDet = n3 * n5 - n2* n6; //Определитель для x
+const xD = n3 * n5 - n2* n6; //Определитель для x
 
-const yDet = n1 * n6 - n3 * n4; // Определитель для y
+const yD = n1 * n6 - n3 * n4; // Определитель для y
 
-const x = xDet / mainDet;
-const  y = yDet / mainDet;
+const x = xD / D;
+const  y = yD / D;
 
-mainDet === 0 ? console.log('Система не имеет единственного решения') : console.log('x = ', x, 'y = ', y);
+D === 0 ? console.log('Система не имеет единственного решения') : console.log('x = ', x, 'y = ', y);
