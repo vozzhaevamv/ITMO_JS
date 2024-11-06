0cbaefe2ab070430e9ad7ff5895923552e08d4f0
vozzhaevamv <mashazhukova2311@gmail.com>
2024-11-06 20:24:07 +1000

    Homework1.js

--- Homework1.js
+++ Homework1.js
@@ -1,19 +1,19 @@
 // Решение системы линейных уравнений методом Крамера
 // Коэффициенты
-const n1 = 3;
-const n2 = 6;
-const n3 = 9;
-const n4 = 12;
-const n5 = 15;
-const n6 = 18;
+const n1 = 2;
+const n2 = 3;
+const n3 = 5;
+const n4 = 1;
+const n5 = 2;
+const n6 = 4;
 
 const D = n1 * n5 - n2 * n4; //Основной определитель
 
-const xD = n3 * n5 - n2* n6; //Определитель для x
+const Dx = n3 * n5 - n2* n6; //Определитель для x
 
-const yD = n1 * n6 - n3 * n4; // Определитель для y
+const Dy = n1 * n6 - n3 * n4; // Определитель для y
 
-const x = xD / D;
-const  y = yD / D;
+const x = Dx / D;
+const  y = Dy / D;
 
 D === 0 ? console.log('Система не имеет единственного решения') : console.log('x = ', x, 'y = ', y);
